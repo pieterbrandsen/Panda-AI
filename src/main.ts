4fa40f77c01c53563370aa6dc7f7451d2bc7017e
Pieter <pieterbrandsen2@gmail.com>
2021-08-26 12:40:40 +0200

    Added errorMapper

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-// #region Game loop
-export const loop = () => {
+import { ErrorMapper } from './errorMapper';
+
+export const loop = ErrorMapper.wrapLoop((): void => {
   Object.values(Game.spawns).forEach(spawn => {
     spawn.spawnCreep([MOVE],"creep");
   });
 
   Object.values(Game.creeps).forEach(creep => {
-    if (creep.room.controller !== undefined) creep.moveTo(creep.room.controller);
     creep.say("Hi",true);
+    if (creep.room.controller !== undefined) creep.moveTo(creep.room.controller);
   });
-
-};
-// #endregion
+});
