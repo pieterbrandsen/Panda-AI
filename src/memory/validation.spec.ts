166fa87841e7d3e8930354a766438fb330f9f776
PieterBrandsen <pieterbrandsen2@gmail.com>
2021-08-31 10:21:36 +0200

    added Unit validation test

---
+++ src/memory/validation.spec.ts
@@ -0,0 +1,33 @@
+import { mockGlobal } from "screeps-jest";
+import VersionedMemoryObjects, {
+  VersionedMemoryName,
+} from "../utils/constants/memory";
+import MemoryValidator from "./validation";
+
+beforeAll(() => {
+  mockGlobal<Memory>("Memory", {});
+  mockGlobal<Game>("Game", {}, true);
+});
+
+describe("Memory validation", () => {
+  describe("Root", () => {
+    it("Should_BeTrue_When_Should_BeMemoryVersionsDoMatch", () => {
+      // Act
+      VersionedMemoryObjects.Root = 1;
+
+      // Assert
+      expect(
+        MemoryValidator.IsMemoryValid(1, VersionedMemoryName.Root)
+      ).toBeTruthy();
+    });
+    it("Should_BeFalse_When_MemoryVersionsDon'tMatch", () => {
+      // Act
+      VersionedMemoryObjects.Root = 2;
+
+      // Assert
+      expect(
+        MemoryValidator.IsMemoryValid(1, VersionedMemoryName.Root)
+      ).toBeFalsy();
+    });
+  });
+});
