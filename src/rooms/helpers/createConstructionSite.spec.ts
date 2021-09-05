dc3639e9dd45794647f26f89fff253602ac2b8d9
Pieter <pieterbrandsen2@gmail.com>
2021-09-05 22:24:38 +0200

    Added room helper with unit tests

---
+++ src/rooms/helpers/createConstructionSite.spec.ts
@@ -0,0 +1,37 @@
+import { mockGlobal, mockInstanceOf } from "screeps-jest";
+import MemoryInitializer from "../../memory/initialization";
+import CreateConstructionSite from "./createConstructionSite";
+
+beforeAll(() => {
+  mockGlobal<Memory>("Memory", {});
+  mockGlobal<Game>("Game", { rooms: {}, structures: {}, creeps: {} }, true);
+  MemoryInitializer.SetupRootMemory();
+});
+const room = mockInstanceOf<Room>({
+  name: "W0N1",
+  find: jest.fn().mockReturnValue([]),
+});
+
+describe("CreateConstructionSite", () => {
+  beforeEach(() => {
+    MemoryInitializer.SetupRoomMemory(room);
+  });
+  it("Should_AddSiteToCsSiteCache_WhenOK", () => {
+    // Arrange
+    room.createConstructionSite = jest.fn().mockReturnValue(OK);
+    const managerName: ManagerNames = "mineral";
+    const cache = Memory.roomsData.data[room.name].managersMemory[managerName];
+
+    // Act
+    CreateConstructionSite(
+      room,
+      new RoomPosition(1, 1, room.name),
+      STRUCTURE_CONTAINER,
+      cache
+    );
+
+    // Assert
+    expect(Object.keys(cache.constructionSites).length).toBe(1);
+    expect(room.createConstructionSite).toHaveLastReturnedWith(OK);
+  });
+});
