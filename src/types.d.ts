8c61196c783417f4110609e8a85304e0d94fade6
Pieter <pieterbrandsen2@gmail.com>
2022-01-14 22:41:52 +0100

    Moved types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,14 +1,11 @@
 interface StringMap<T> {
   [key: string]: T;
 }
-
-interface MainMemoryData<MemoryType,CacheType> {
-  data: StringMap<MemoryType>;
-  version: number;
-  cache: StringMap<CacheType>
+interface ValidatedData {
+  isValid: boolean;
+  nonValidObjects: string[];
 }
-interface Memory {
-  CreepsData: MainMemoryData<CreepMemory, CreepCache>;
-  StructuresData: MainMemoryData<StructureMemory, StructureCache>;
-  RoomsData: MainMemoryData<RoomMemory, RoomCache>;
+
+interface Predicate<T> {
+  (data:T):boolean;
 }
