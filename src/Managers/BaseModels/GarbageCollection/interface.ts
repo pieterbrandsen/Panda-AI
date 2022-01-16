0d1e7be9796e84da05a2d19a3d6ef097bf318901
Pieter <pieterbrandsen2@gmail.com>
2022-01-16 22:02:40 +0100

    Updated required functions

--- src/Managers/BaseModels/GarbageCollection/interface.ts
+++ src/Managers/BaseModels/GarbageCollection/interface.ts
@@ -1,11 +1,11 @@
-//* Types of cache: Creep, Room, Global, Jobs
+//* Types of cache: Creep, Room
 //* For each are the following functions
-// TODO: Validate
-// TODO: Create object
-// TODO: Get (from path, or by id)
-// TODO: Add (to path)
-// TODO: Delete (to path, option to revert back to original path)
+// TODO: Validate (all/single)
+// TODO: Generate
+// TODO: CRUD
+// TODO: Revert to original location (dont update if already present) then remove from memory
 
-interface IGarbageCollection {}
+interface IGarbageCollection {
+}
 
 export default class implements IGarbageCollection {}
