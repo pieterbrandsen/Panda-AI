6857082132d11b53e5ac5360f07b61f441bdb3e8
Pieter <pieterbrandsen2@gmail.com>
2022-01-29 00:07:37 +0100

    Added function to get open spots around structure

--- src/Managers/BaseModels/Helper/Room/roomPosition.ts
+++ src/Managers/BaseModels/Helper/Room/roomPosition.ts
@@ -16,4 +16,26 @@ export default class implements IRoomPosition {
   static GetMiddlePosition(roomName: string): FreezedRoomPosition {
     return { x: 25, y: 25, roomName };
   }
+
+  static GetNonWallPositionsAround(
+    pos: FreezedRoomPosition,
+    room: Room,
+    maxDistance: number = 1
+  ): number {
+    const nonWallPositions = room
+      .lookForAtArea(
+        LOOK_TERRAIN,
+        pos.y - maxDistance,
+        pos.x - maxDistance,
+        pos.y + maxDistance,
+        pos.x + maxDistance,
+        true
+      )
+      .filter(
+        (terrain) =>
+          terrain.terrain !== "wall" &&
+          (terrain.x !== pos.x || terrain.y !== pos.y)
+      );
+    return nonWallPositions.length;
+  }
 }
