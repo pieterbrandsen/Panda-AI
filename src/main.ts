e452c798c17743b4da6c1af37b438b5d4a4650e2
Pieter <pieterbrandsen2@gmail.com>
2022-02-02 11:57:02 +0100

    Added market that always sells cpu unlock if higher then 40M

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,20 @@ import IRoomsExecuter from "./Executers/Room/interface";
 
 // eslint-disable-next-line
 export const loop = ErrorMapper.wrapLoop((): void => {
+  // const orders = Game.market.getAllOrders(order => order.resourceType == CPU_UNLOCK &&
+  //   order.type == ORDER_BUY &&
+  //   order.price > 40 * 1000 * 1000);
+  
+  // for(let i=0; i<orders.length; i++) {
+  //   const order = orders[i];
+  //   const result = Game.market.deal(order.id, 100);
+  //   if (result == OK) {
+  //     const message = `Dealed ${order.amount} for ${order.price}`;
+  //     Game.notify(message,0);
+  //     console.log(message);      
+  //   }
+  // }
+
   if (!IGlobalMemory.ValidateSingle()) {
     IGlobalData.Initialize();
     return;
